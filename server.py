f39f215c5a49f05e038ef501770d92276f12c93a
Peder Bergebakken Sundt <pbsds@hotmail.com>
2017-02-05 17:10:50 +0100

    updated server.py to use the newest version of REMI and added a nice PVV logo in the background

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
-import remi.gui as gui, random, os, time, shutil, sys
-from remi import start, App
+#!/usr/bin/env python3.6
+import random, os, time, shutil, sys
 from threading import Timer
-if sys.version_info[0] == 2:
-	from ConfigParser import ConfigParser
-else:
-	from configparser import ConfigParser
+import remi.gui as gui
+from remi import start, App
+from grzegorz import nyasync
+from configparser import ConfigParser
+
 if "--no-mpv" not in sys.argv:
 	from mpv_control import mpv
 
@@ -34,14 +34,15 @@ def get_youtube_metadata(url, ydl = youtube_dl.YoutubeDL()):
 
 class MyApp(App):
 	def __init__(self, *args):
-		res_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'res')
-		super(MyApp, self).__init__(*args, static_paths=(res_path,))
-		
+		res_path = os.path.join(os.path.dirname(__file__), 'res')
+		super(MyApp, self).__init__(*args, static_file_path=res_path)
 		
 		#initalize mpv here?
 		
 	def main(self):
 		container = gui.VBox(width=512)
+		container.style["margin-left"] = "auto"
+		container.style["margin-right"] = "auto"
 		
 		#logo:
 		container.append(gui.Image('/res/logo.jpg', width=512))
@@ -53,7 +54,7 @@ class MyApp(App):
 			button = gui.Button(i.capitalize(), margin="5px")
 			setattr(self.playback, i, button)
 			playbackContainer.append(button)
-			button.set_on_click_listener(self, 'playback_%s' % i)
+			button.set_on_click_listener(getattr(self,'playback_%s' % i))
 		self.playback.playing = gui.Label("Now playing: None")
 		self.playback.slider = gui.Slider(0, 0, 100, 1, width="85%", height=20, margin='10px')
 		
@@ -64,7 +65,8 @@ class MyApp(App):
 		#playlist
 		self.playlist = namespace()
 		self.playlist.table = gui.Table(width="100%", margin="10px")
-		self.playlist.table.from_2d_matrix([['#', 'Name', "length"]])
+		self.playlist.table.append_from_list([['#', 'Name', "length"]], fill_title=True)
+		
 		container.append(self.playlist.table)
 		
 		self.playlist.queue = []#[i] = [source, name, length]
@@ -74,11 +76,11 @@ class MyApp(App):
 		inputContainer = gui.HBox(width=512)
 		self.input = namespace()
 		self.input.field = gui.TextInput(single_line=True, height="20px", margin="5px")
-		self.input.field.style["border"] = "1px solid %s" % COLOR_BLUE
+		self.input.field.style["border"]     = "1px solid %s" % COLOR_BLUE
 		self.input.field.style["box-shadow"] = "0px 0px 5px 0px %s" % COLOR_BLUE_SHADOW
 		self.input.submit = gui.Button("Submit!", margin="5px")
-		self.input.field.set_on_enter_listener(self, "input_submit")
-		self.input.submit.set_on_click_listener(self, "input_submit")
+		self.input.field.set_on_enter_listener(self.input_submit)
+		self.input.submit.set_on_click_listener(self.input_submit)
 		
 		inputContainer.append(self.input.field)
 		inputContainer.append(self.input.submit)
@@ -92,23 +94,28 @@ class MyApp(App):
 		
 		self.playback_update()
 		
-		self.playlist.table.from_2d_matrix(self.playlist_update())
+		
+		self.playlist.table.empty(keep_title=True)
+		self.playlist.table.append_from_list(self.playlist_update())
 		
 		Timer(1, self.mainLoop).start()
-	def playback_play(self): pass
-	def playback_pause(self): pass
-	def playback_next(self):
+	#events:
+	def playback_play(self, widget): pass
+	def playback_pause(self, widget): pass
+	def playback_next(self, widget):
 		source, name, length = self.playlist.queue.pop(0)
 		
 		pass
 		
-	def input_submit(self, value=None):
+	def input_submit(self, widget, value=None):
 		if not value:
 			value = self.input.field.get_text()
+		self.input.field.set_text("")
 		
 		title, length =  get_youtube_metadata(value)
 		
-		self.input.field.set_text("")
+		self.playlist.queue.append([value, title, length])
+		
 	#playback steps:
 	def playback_update(self):
 		#talk to mpv, see wether the song is being played still
@@ -120,13 +127,14 @@ class MyApp(App):
 		
 		return
 	def playlist_update(self):
-		out = [['#', 'Name', "length"]]
-		
+		#out = [['#', 'Name', "length"]]
+		out = []
 		for i, (source, name, length) in enumerate(self.playlist.queue):
 			out.append([str(i+1), name, length])
 		
 		return out
 
+#@nyasync.ify
 def main():
 	if not os.path.exists("config.ini"):
 		shutil.copyfile("default_config.ini", "config.ini")
@@ -141,7 +149,7 @@ def main():
 	multiple_instance = ini.getboolean("server", "multiple_instance")
 	
 	# starts the webserver
-	start(MyApp, address=host, port=port, start_browser=start_browser, multiple_instance=multiple_instance)
+	start(MyApp, title="Gregorz", address=host, port=port, start_browser=start_browser, multiple_instance=multiple_instance, enable_file_cache=True)
 
 if __name__ == "__main__":
-		main()
+	main()
