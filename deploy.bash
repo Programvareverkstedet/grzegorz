2f130874f761819c870a3edb08cfd0090135a84d
Aleksander Wasaznik <aleksander@wasaznik.com>
2017-02-06 20:20:25 +0100

    Update deploy to match new systemd setup
    Grezgorz now runs as a service on the user instance of systemd.

--- deploy.bash
+++ deploy.bash
@@ -2,23 +2,23 @@
 # My little crappy deploy script
 # Uploads all files not ignored by git
 
-TARGET=brzeczyszczykiewicz.pvv.ntnu.no
+TARGET=grzegorz@brzeczyszczykiewicz.pvv.ntnu.no
 TARGET_PATH='grzegorz'
 
 array=(); while IFS= read -rd '' item; do array+=("$item"); done < \
 	<(git status -z --short | grep -z ^? | cut -z -d\  -f2-; git ls-files -z)
 files_not_ignored=("${array[@]}")
 
-ssh -T "grzegorz@$TARGET" "
+ssh -T "$TARGET" "
 	rm -rf $TARGET_PATH
 	mkdir -p $TARGET_PATH
 	"
 
 tar -c "${files_not_ignored[@]}" |
-	ssh -T "grzegorz@$TARGET" "
+	ssh -T "$TARGET" "
 	tar -vxC $TARGET_PATH
 	"
-ssh -T "root@$TARGET" "
-	systemctl restart grzegorz
-	systemctl status grzegorz
+
+ssh -T "$TARGET" "
+	systemctl --user restart grzegorz@0
 	"
